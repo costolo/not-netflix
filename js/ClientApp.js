0f9f72c48dce9d9f14a3d40ff972d04f034c0733
Ben <ben.costolo@gmail.com>
2016-10-12 14:20:04 -0400

    commit

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,41 +1,43 @@
+/* global React ReactDOM */
+
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
 var MyTitle = React.createClass({
-	//enhanced object literal syntax
-	render() {
-		return (
-			div(null,
-				h1({
-					style: {
-						color: this.props.color
-					}
-				}, this.props.title)
-			)
-		)
-	}
+  // enhanced object literal syntax
+  render() {
+    return (
+      div(null,
+        h1({
+          style: {
+            color: this.props.color
+          }
+        }, this.props.title)
+      )
+    )
+  }
 })
 
 var ce = React.createElement
 var cf = React.createFactory(MyTitle)
 
 var MyFirstComponent = (
-	div(null,
-		React.createElement(MyTitle, {
-			title: 'prop 0',
-			color: 'blanchedalmond'
-		}),
+  div(null,
+    React.createElement(MyTitle, {
+      title: 'prop 0',
+      color: 'blanchedalmond'
+    }),
 
-		cf({
-			title: 'prop1',
-			color: 'mediumseagreen'
-		}),
+    cf({
+      title: 'prop1',
+      color: 'mediumseagreen'
+    }),
 
-		ce(MyTitle, {
-			title: 'prop2',
-			color: 'peru'
-		})
-	)
+    ce(MyTitle, {
+      title: 'prop2',
+      color: 'peru'
+    })
+  )
 )
 
 ReactDOM.render(MyFirstComponent, document.getElementById('app'))
