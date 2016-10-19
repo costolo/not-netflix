3fb617aa5db3bbf2ad04b213d3793dd3c2242331
Ben <ben.costolo@gmail.com>
2016-10-19 11:12:10 -0400

    comment components require section and add search component route

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,11 +1,15 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
-const Landing = require('./Landing')
 const { Router, Route, hashHistory } = require('react-router')
+
+// components
+const Search = require('./Search')
+const Landing = require('./Landing')
 
 const App = () => (
   <Router history={hashHistory}>
     <Route path='/' component={Landing} />
+    <Route path='/search' component={Search} />
   </Router>
 )
 
