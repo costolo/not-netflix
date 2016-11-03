f4735d1bf71dde0925a23cc520e5cb6207e7d82c
Ben <ben.costolo@gmail.com>
2016-11-03 13:06:49 -0400

    add Details component and associated route

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -6,12 +6,14 @@ const { Router, Route, hashHistory, IndexRoute } = require('react-router')
 const Search = require('./Search')
 const Landing = require('./Landing')
 const Layout = require('./Layout')
+const Details = require('./Details')
 
 const App = () => (
   <Router history={hashHistory}>
     <Route path='/' component={Layout}>
       <IndexRoute component={Landing} />
       <Route path='/search' component={Search} />
+      <Route path='/details/:id' component={Details} />
     </Route>
   </Router>
 )
