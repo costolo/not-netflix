febe13f1cb4b7ba5ae3df1f754c2a9369f09b268
Ben <ben.costolo@gmail.com>
2016-12-13 17:40:45 -0500

    refactor App component back into a stateless functional component, and remove show logic (to be handled by redux)

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { Router, Route, hashHistory, IndexRoute } from 'react-router'
-import { shows } from '../public/data'
-
 // components
 import Search from './Search'
 import Landing from './Landing'
@@ -11,31 +8,16 @@ import Details from './Details'
 import { store } from './Store'
 import { Provider } from 'react-redux'
 
-const App = React.createClass({
-  assignShow (nextState, replace) {
-    const showArray = shows.filter((show) => show.imdbID === nextState.params.id)
+const App = () => (
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path='/' component={Layout}>
+        <IndexRoute component={Landing} />
+        <Route path='/search' component={Search} />
+        <Route path='/details/:id' component={Details} />
+      </Route>
+    </Router>
+  </Provider>
+)
 
-    if (showArray.length > 1) {
-      return replace('/')
-    }
-
-    Object.assign(nextState.params, showArray[0])
-    return nextState
-  },
-
-  render () {
-    return (
-      <Provider store={store}>
-        <Router history={hashHistory}>
-          <Route path='/' component={Layout}>
-            <IndexRoute component={Landing} />
-            <Route path='/search' component={Search} shows={shows} />
-            <Route path='/details/:id' component={Details} onEnter={this.assignShow} />
-          </Route>
-        </Router>
-      </Provider>
-    )
-  }
-})
-
-ReactDOM.render(<App />, document.getElementById('app'))
+export default App
