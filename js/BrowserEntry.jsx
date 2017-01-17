2e291d588b5852f8487d7501dd651cecedcc8411
Ben <ben.costolo@gmail.com>
2017-01-17 11:07:51 -0500

    Use match to handle async routing

--- js/BrowserEntry.jsx
+++ js/BrowserEntry.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './ClientApp'
+import { match } from 'react-router'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+match({
+  history: App.History,
+  routes: App.Routes
+}, (err, redirectLocation, renderProps) => {
+  if (err) return console.error('browser entry error')
+  ReactDOM.render(<App {...renderProps} />, document.getElementById('app'))
+})
+
