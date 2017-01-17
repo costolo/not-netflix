4387aa423b39ada52b232cd840658ab3cb24af09
Ben <ben.costolo@gmail.com>
2017-01-17 11:08:15 -0500

    replace stateless functional route component with configuration object, shim node-ensure for use in node, and add browserHistory to App component

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,26 +1,55 @@
 import React from 'react'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
-import Search from './Search'
-import Landing from './Landing'
 import Layout from './Layout'
-import Details from './Details'
+import { Router, browserHistory } from 'react-router'
 import { store } from './Store'
 import { Provider } from 'react-redux'
 
-const Routes = () => (
-  <Route path='/' component={Layout}>
-    <IndexRoute component={Landing} />
-    <Route path='/search' component={Search} />
-    <Route path='/details/:id' component={Details} />
-  </Route>
-)
+if (typeof module !== 'undefined' && module.require) {
+  if (typeof require.ensure === 'undefined') {
+    require.ensure = require('node-ensure') // shim for node.js
+  }
+}
+
+const rootRoute = {
+  component: Layout,
+  path: '/',
+  indexRoute: {
+    getComponent (location, cb) {
+      require.ensure([], (err) => {
+        console.error('require.ensure error', err)
+        cb(null, require('./Landing').default)
+      })
+    }
+  },
+  childRoutes: [
+    {
+      path: 'search',
+      getComponent (location, cb) {
+        require.ensure([], (err) => {
+          console.error('require.ensure error', err)
+          cb(null, require('./Search').default)
+        })
+      }
+    },
+    {
+      path: 'details/:id',
+      getComponent (location, cb) {
+        require.ensure([], (err) => {
+          console.error('require.ensure error', err)
+          cb(null, require('./Details').default)
+        })
+      }
+    }
+  ]
+}
 
 const App = () => (
   <Provider store={store}>
-    <Router history={browserHistory} routes={Routes()} />
+    <Router history={browserHistory} routes={rootRoute} />
   </Provider>
 )
 
-App.Routes = Routes
+App.Routes = rootRoute
+App.History = browserHistory
 
 module.exports = App
